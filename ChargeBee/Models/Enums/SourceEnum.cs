478cc8afeffdee8454e102dc173b9889c36cfc20
rraman <rr@chargebee.com>
2013-08-19 16:54:09 +0530

    Enums for the event source attribute

---
+++ ChargeBee/Models/Enums/SourceEnum.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel;
+
+namespace ChargeBee.Models.Enums
+{
+    public enum SourceEnum
+    {
+
+        [Description("Unknown Enum")]
+        UnKnown, /*Indicates unexpected value for this enum. You can get this when there is a
+                dotnet-client version incompatibility. We suggest you to upgrade to the latest version */
+
+        [Description("admin_console")]
+        AdminConsole,
+
+        [Description("api")]
+        Api,
+
+        [Description("scheduled_job")]
+        ScheduledJob,
+
+        [Description("hosted_page")]
+        HostedPage,
+
+        [Description("system")]
+        System,
+
+        [Description("none")]
+        None,
+
+    }
+}
