dbc92632a7d935665f60b67dd7c85b4afbc50e10
sangeetha <sangeetha@chargebee.com>
2016-07-06 08:01:34 +0000

    * * * ** APIs added**: A new endpoint to delete a coupon added. See : https://apidocs.chargebee.com/docs/api/coupons#delete_a_coupon

--- ChargeBee/Api/ApiConfig.cs
+++ ChargeBee/Api/ApiConfig.cs
@@ -7,7 +7,7 @@ namespace ChargeBee.Api
     {
 		public static string DomainSuffix = "chargebee.com";
 		public static string Proto = "https";
-		public static string Version = "2.0.8";
+		public static string Version = "2.0.9";
 		public static readonly string API_VERSION = "v2";
 
 		public string ApiKey { get; set; }
--- ChargeBee/Models/Coupon.cs
+++ ChargeBee/Models/Coupon.cs
@@ -34,6 +34,11 @@ namespace ChargeBee.Models
             string url = ApiUtil.BuildUrl("coupons", CheckNull(id));
             return new EntityRequest<Type>(url, HttpMethod.GET);
         }
+        public static EntityRequest<Type> Delete(string id)
+        {
+            string url = ApiUtil.BuildUrl("coupons", CheckNull(id), "delete");
+            return new EntityRequest<Type>(url, HttpMethod.POST);
+        }
         #endregion
         
         #region Properties
@@ -323,6 +328,8 @@ namespace ChargeBee.Models
             Expired,
             [Description("archived")]
             Archived,
+            [Description("deleted")]
+            Deleted,
 
         }
         [Obsolete]
